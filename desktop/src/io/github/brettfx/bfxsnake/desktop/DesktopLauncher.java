0f14dd1c399a2084cbc79967416cd2882404128c
Brett Allen <brettallen777@gmail.com>
2017-12-23 01:03:16 -0500

    Used libGDX starter program to generate basis for snake project, i.e., initial commit

---
+++ desktop/src/io/github/brettfx/bfxsnake/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.github.brettfx.bfxsnake.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.github.brettfx.bfxsnake.BFXSnake;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BFXSnake(), config);
+	}
+}
