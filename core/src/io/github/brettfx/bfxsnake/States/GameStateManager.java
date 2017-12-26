ff15e644b3acd19bde774624a7f13964c7faca28
Brett Allen <brettallen777@gmail.com>
2017-12-26 12:23:09 -0500

    Implemented GameStateManager

--- core/src/io/github/brettfx/bfxsnake/States/GameStateManager.java
+++ core/src/io/github/brettfx/bfxsnake/States/GameStateManager.java
@@ -1,9 +1,41 @@
 package io.github.brettfx.bfxsnake.States;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import java.util.Stack;
+
 /**
  * @author Brett Allen
  * @since 12/24/2017
  */
 
 public class GameStateManager {
+
+    //A stack of states for more efficient memory utilization
+    private Stack<State> m_states;
+
+    public GameStateManager(){
+        m_states = new Stack<State>();
+    }
+
+    public void push(State state){
+        m_states.push(state);
+    }
+
+    public void pop(){
+        m_states.pop();
+    }
+
+    public void set(State state){
+        m_states.pop();
+        m_states.push(state);
+    }
+
+    public void update(float dt){
+        m_states.peek().update(dt);
+    }
+
+    public void render(SpriteBatch sb){
+        m_states.peek().render(sb);
+    }
 }
