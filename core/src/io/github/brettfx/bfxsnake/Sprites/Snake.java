81593e1798d3cb9ea157c85952d7432f62c240a9
Brett Allen <brettallen777@gmail.com>
2017-12-26 12:51:08 -0500

    Implemented SnakePart class

--- core/src/io/github/brettfx/bfxsnake/Sprites/Snake.java
+++ core/src/io/github/brettfx/bfxsnake/Sprites/Snake.java
@@ -1,5 +1,10 @@
 package io.github.brettfx.bfxsnake.Sprites;
 
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
+
 /**
  * @author brett
  * @since 12/25/2017
@@ -8,5 +13,32 @@ package io.github.brettfx.bfxsnake.Sprites;
 public class Snake {
     public static final boolean DEBUG_MODE = false;
 
+    private static final int INIT_MOVEMENT_SPEED = 100;
+
+    private Vector3 m_position;
+    private Vector3 m_velocity;
+    private Rectangle m_bounds;
+
+    private Array<SnakePart> m_snakeParts;
+
+    //To render individual snake parts
+    private ShapeRenderer m_shapeRenderer;
+
+    public boolean m_colliding;
+
+    /**
+     * Constructor
+     *
+     * @param xLoc the x-coordinate location to spawn the snake
+     * @param yLoc the y-coordinate location to spawn the snake
+     * */
+    public Snake(int xLoc, int yLoc){
+        m_position = new Vector3(xLoc, yLoc, 0);
+        m_velocity = new Vector3(0, 0, 0);
+
+
+
+        m_colliding = false;
+    }
 
 }
--- core/src/io/github/brettfx/bfxsnake/Sprites/SnakePart.java
+++ core/src/io/github/brettfx/bfxsnake/Sprites/SnakePart.java
@@ -1,5 +1,10 @@
 package io.github.brettfx.bfxsnake.Sprites;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Rectangle;
+
+import io.github.brettfx.bfxsnake.BFXSnake;
+
 /**
  * @author brett
  * @since 12/25/2017
@@ -7,4 +12,52 @@ package io.github.brettfx.bfxsnake.Sprites;
  * A snake is comprised of snake parts
  */
 public class SnakePart {
+    private Rectangle m_part;
+    private float m_xLoc;
+    private float m_yLoc;
+    private float m_width;
+    private float m_height;
+
+    /**
+     * Default constructor to be used at initial state of game
+     * Makes it so the snake will spawn in the center of the screen
+     * Dimensions of snake part depend on the width and height of screen and the
+     * scale factor set in the BGXSnake class
+     * */
+    public SnakePart(){
+        m_width = Gdx.graphics.getWidth() / BFXSnake.SCALE_FACTOR;
+        m_height = Gdx.graphics.getHeight() / BFXSnake.SCALE_FACTOR;
+        m_xLoc = m_width / 2;
+        m_yLoc = m_height / 2;
+        m_part = new Rectangle(m_xLoc, m_yLoc, m_width, m_height);
+
+    }
+
+    public SnakePart(float x, float y, float width, float height){
+        m_part = new Rectangle(x, y, width, height);
+        m_xLoc = x;
+        m_yLoc = y;
+        m_width = width;
+        m_height = height;
+    }
+
+    public Rectangle getPart(){
+        return m_part;
+    }
+
+    public float getX() {
+        return m_xLoc;
+    }
+
+    public float getY() {
+        return m_yLoc;
+    }
+
+    public float getWidth() {
+        return m_width;
+    }
+
+    public float getHeight() {
+        return m_height;
+    }
 }
