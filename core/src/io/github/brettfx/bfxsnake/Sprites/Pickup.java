d053a475f4277fbee6f8ca3c97866b08967ad93a
Brett Allen <brettallen777@gmail.com>
2017-12-26 00:00:16 -0500

    Added pickup and snake part classes and implemented animation class

---
+++ core/src/io/github/brettfx/bfxsnake/Sprites/Pickup.java
@@ -0,0 +1,12 @@
+package io.github.brettfx.bfxsnake.Sprites;
+
+/**
+ * @author brett
+ * @since 12/25/2017
+ *
+ * Pick ups are what the snake is supposed to eat to grow
+ * Each time a pick up is eattn by the snake, the snake will grow in size.
+ * Once the snake has eaten all the pickups a special prize will be won.
+ */
+public class Pickup {
+}
---
+++ core/src/io/github/brettfx/bfxsnake/Sprites/SnakePart.java
@@ -0,0 +1,10 @@
+package io.github.brettfx.bfxsnake.Sprites;
+
+/**
+ * @author brett
+ * @since 12/25/2017
+ *
+ * A snake is comprised of snake parts
+ */
+public class SnakePart {
+}
