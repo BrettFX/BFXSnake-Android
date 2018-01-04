8bd2bbeffabd261301553ee1e0b887456edf2427
Brett Allen <brettallen777@gmail.com>
2018-01-03 22:05:26 -0500

    Preparing to implement settings state

---
+++ core/src/io/github/brettfx/bfxsnake/States/SettingsState.java
@@ -0,0 +1,33 @@
+package io.github.brettfx.bfxsnake.States;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * @author brett
+ * @since 1/3/2018
+ */
+public class SettingsState extends State{
+    protected SettingsState(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    public void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
