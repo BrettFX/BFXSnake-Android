c5f3736bed8ff6c9976f6eb5747d788ad6633a1f
Brett Allen <brettallen777@gmail.com>
2018-01-23 00:26:30 -0500

    Successfully converted button textures to text buttons for menu

--- core/src/io/github/brettfx/bfxsnake/States/MenuState.java
+++ core/src/io/github/brettfx/bfxsnake/States/MenuState.java
@@ -38,11 +38,10 @@ public class MenuState extends State {
     private Stage m_menuStage;
     private BitmapFont m_menuFont;
 
-    private Label m_settingsLabel;
-    private Label m_highScoreLabel;
-    private Label m_difficultyLabel;
-
     private TextButton m_btnPlay;
+    private TextButton m_btnSettings;
+    private TextButton m_btnHighScore;
+    private TextButton m_btnDifficulty;
 
     public MenuState(GameStateManager gsm) {
         super(gsm);
@@ -61,8 +60,6 @@ public class MenuState extends State {
         m_menuFont = new BitmapFont(Gdx.files.internal(BFXSnake.MENU_FONT));
         m_menuFont.getData().setScale(FONT_SIZE, FONT_SIZE);
 
-        Label.LabelStyle menuLabelStyle = new Label.LabelStyle(m_menuFont, m_menuFont.getColor());
-
         m_btnPlay = new TextButton("PLAY", m_gsm.getButtonStyle());
         m_btnPlay.pad(BUTTON_PADDING); //Adds padding around text to give feal of authentic button
         m_btnPlay.addListener(new InputListener(){
@@ -77,8 +74,9 @@ public class MenuState extends State {
             }
         });
 
-        m_settingsLabel = new Label("SETTINGS", menuLabelStyle);
-        m_settingsLabel.addListener(new InputListener(){
+        m_btnSettings = new TextButton("SETTINGS", m_gsm.getButtonStyle());
+        m_btnSettings.pad(BUTTON_PADDING);
+        m_btnSettings.addListener(new InputListener(){
             @Override
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button){
                 return true;
@@ -91,9 +89,10 @@ public class MenuState extends State {
             }
         });
 
-        m_highScoreLabel =  new Label("HIGH SCORE: " + String.format(Locale.getDefault(), "%02d",
-                Score.getHighScore()), menuLabelStyle);
-        m_highScoreLabel.addListener(new InputListener(){
+        m_btnHighScore = new TextButton("HIGH SCORE: " + String.format(Locale.getDefault(), "%02d",
+                Score.getHighScore()), m_gsm.getButtonStyle());
+        m_btnHighScore.pad(BUTTON_PADDING);
+        m_btnHighScore.addListener(new InputListener(){
             @Override
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
                 return true;
@@ -106,8 +105,9 @@ public class MenuState extends State {
             }
         });
 
-        m_difficultyLabel =  new Label("DIFFICULTY: " + DIFFICULTIES[m_gsm.getDifficulty()], menuLabelStyle);
-        m_difficultyLabel.addListener(new InputListener(){
+        m_btnDifficulty = new TextButton("DIFFICULTY: " + DIFFICULTIES[m_gsm.getDifficulty()], m_gsm.getButtonStyle());
+        m_btnDifficulty.pad(BUTTON_PADDING);
+        m_btnDifficulty.addListener(new InputListener(){
             @Override
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button){
                 return true;
@@ -120,7 +120,7 @@ public class MenuState extends State {
                 System.out.println("Current difficulties: " + DIFFICULTIES[m_gsm.getDifficulty()] +
                         " (" + m_gsm.getDifficulty() + ")");
 
-                m_difficultyLabel.setText("DIFFICULTY: " + DIFFICULTIES[m_gsm.getDifficulty()]);
+                m_btnDifficulty.setText("DIFFICULTY: " + DIFFICULTIES[m_gsm.getDifficulty()]);
             }
         });
 
@@ -138,7 +138,7 @@ public class MenuState extends State {
         selectionTable.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         selectionTable.center();
 
-        selectionTable.top().padTop(Gdx.graphics.getHeight() / 4);
+        selectionTable.top().padTop(Gdx.graphics.getHeight() / 6);
         selectionTable.setFillParent(true);
 
         float padding = Gdx.graphics.getWidth() / BFXSnake.SCALE_FACTOR;
@@ -150,13 +150,13 @@ public class MenuState extends State {
         selectionTable.add(m_btnPlay).width(largeLabel.getWidth() * BUTTON_WIDTH_SCALE);
         selectionTable.row().padTop(padding);
 
-        selectionTable.add(m_settingsLabel).expandX();
+        selectionTable.add(m_btnSettings).width(largeLabel.getWidth() * BUTTON_WIDTH_SCALE);
         selectionTable.row().padTop(padding);
 
-        selectionTable.add(m_highScoreLabel).expandX();
+        selectionTable.add(m_btnHighScore).width(largeLabel.getWidth() * BUTTON_WIDTH_SCALE);
         selectionTable.row().padTop(padding);
 
-        selectionTable.add(m_difficultyLabel).expandX();
+        selectionTable.add(m_btnDifficulty).width(largeLabel.getWidth() * BUTTON_WIDTH_SCALE);
 
         m_menuStage.addActor(selectionTable);
 
